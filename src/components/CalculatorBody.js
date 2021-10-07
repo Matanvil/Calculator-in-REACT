a7682291cb12f41d34eb537e7974eac5d87d4407
matanv <matanv@gmail.com>
2021-10-08 00:09:17 +0300

    changes to display component and calculator body

--- src/components/CalculatorBody.js
+++ src/components/CalculatorBody.js
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import CalculatorOperatorsList from "./CalculatorOperatorsList";
 import CalculatorNumbersList from "./CalculatorNumbersList";
+import CalculatorDisplay from "./CalculatorDisplay";
 
 function CalculatorBody() {
+  const [value, setValue] = useState(5)
   return (
     <div className="calculator-body">
-      <CalculatorOperatorsList onSelected={(operator) => alert("operator selected " + operator) }/> 
-      <CalculatorNumbersList onSelected={(digit) => alert("digit selected " + digit) }/> 
+      <CalculatorOperatorsList onSelected={(operator) => setValue(operator) }/> 
+      <CalculatorNumbersList onSelected={(digit) => setValue(digit) }/> 
+      <CalculatorDisplay value={} /> 
     </div>
   );
 }
--- src/components/CalculatorDisplay.js
+++ src/components/CalculatorDisplay.js
@@ -1,11 +1,11 @@
+import { useState } from "react";
 
 
-function CalculatorDisplay() {
+function CalculatorDisplay(value) {
   return (
     <label>
-      <input className="display" type="text" />
+      <input className="display" type="text" value={value} />
     </label>
   );
 }
-
 export default CalculatorDisplay;
